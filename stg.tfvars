13d774704fab36a7795199549ca4e33888ee8cc3
AishaMohamed2022 <107565892+AishaMohamed2022@users.noreply.github.com>
2026-09-12 11:05:27 +0300

    Add files via upload

---
+++ stg.tfvars
@@ -0,0 +1,8 @@
+vpc_cidr="172.17.0.0/16"
+pub1_cidr="172.17.1.0/24"
+pub2_cidr="172.17.3.0/24"
+private1_cidr="172.17.2.0/24"
+private2_cidr="172.17.4.0/24"
+availability_zone1 = "us-east-1a"
+availability_zone2 = "us-east-1b"
+env = "stg"
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "pub1_cidr" {
+  type = string
+}
+
+variable "pub2_cidr" {
+  type = string
+}
+
+variable "private1_cidr" {
+  type = string
+}
+
+variable "private2_cidr" {
+  type = string
+}
+variable "availability_zone1" {
+  type = string
+}
+variable "availability_zone2" {
+  type = string
+}

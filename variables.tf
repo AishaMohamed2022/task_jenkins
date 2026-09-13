1ed7ffb3e84cf602ceb840602bddd4fab81ae8ba
AishaMohamed2022 <107565892+AishaMohamed2022@users.noreply.github.com>
2026-09-13 23:16:35 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,39 @@
 variable "vpc_cidr" {
-  type = string
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "env" {
-  type = string
+  type    = string
+  default = "dev"
 }
 
 variable "pub1_cidr" {
-  type = string
+  type    = string
+  default = "10.0.1.0/24"
 }
 
 variable "pub2_cidr" {
-  type = string
+  type    = string
+  default = "10.0.2.0/24"
 }
 
 variable "private1_cidr" {
-  type = string
+  type    = string
+  default = "10.0.3.0/24"
 }
 
 variable "private2_cidr" {
-  type = string
+  type    = string
+  default = "10.0.4.0/24"
 }
+
 variable "availability_zone1" {
-  type = string
+  type    = string
+  default = "us-east-1a"
 }
+
 variable "availability_zone2" {
-  type = string
+  type    = string
+  default = "us-east-1b"
 }

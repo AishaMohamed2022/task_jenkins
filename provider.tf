c859cdb923ad93335ec19037a6bd1c9bd1f16b0f
AishaMohamed2022 <107565892+AishaMohamed2022@users.noreply.github.com>
2026-09-14 13:19:23 +0300

    Update provider.tf new

--- provider.tf
+++ provider.tf
@@ -1,13 +1,14 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "test"
-  secret_key = "test"
-
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
-
+  region                      = "us-east-1"
+  access_key                  = "mock_access_key" 
+  secret_key                  = "mock_secret_key" 
   
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    ec2 = "http://localhost:4566" 
+    vpc = "http://localhost:4566"
+  }
 }
-
-
